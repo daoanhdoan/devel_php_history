56cdab3b0a2a6625d56b29924375ff50922aa691
daoanhdoan <daoanhdoan@yahoo.com>
2021-08-02 21:56:12 +0700

    first commit

---
+++ js/devel_php_history.js
@@ -0,0 +1,10 @@
+'use strict';
+
+(function ($) {
+  // To understand behaviors, see https://drupal.org/node/756722#behaviors
+  Drupal.behaviors.develPhpHistory = {
+    attach: function (context, settings) {
+
+    }
+  }
+})(jQuery);
---
+++ src/Controller/DevelPhpHistoryController.php
@@ -0,0 +1,10 @@
+<?php
+namespace Drupal\devel_php_history\Controller;
+
+use Drupal\Core\Controller\ControllerBase;
+
+class DevelPhpHistoryController extends ControllerBase {
+  public function updateArchive() {
+
+  }
+}
